7c95568c8963d2d11abb52566c7346cbfdecd759
tompvr26 <tompoyvre@gmail.com>
2021-03-14 12:11:58 +0100

    update views, add v-model

--- src/views/Films.vue
+++ src/views/Films.vue
@@ -1,13 +1,20 @@
 <template>
+
   <div class="films">
     <h2>Films</h2>
-    <div id="demo" class="container">
-      <input type="text" placeholder="Entrez un nom de film" id="name">
-      <button type="button" onclick="getFilm();">Envoyer les informations</button>
+    <div id="films" class="container">
+      <label for="filmName">Entrez un nom de film</label> <br>
+      <input type="text" v-model="searchQuery" placeholder="ex: Intouchable" id="filmName">
+      <br> <br>
+      <label for="date">Entrez la date de sortie (AAAA-MM-JJ)</label> <br>
+      <input type="text" v-model="searchDate" placeholder="ex: 2011-11-02" id="date">
+      <br> <br>
+      <button type="submit" @click="getFilms">Envoyer les informations</button>
 
+      <hr>
     </div>
     <p>
-      <!--{{ getFilm() }}-->
+      <!--{{ getFilms() }}-->
     </p>
 
   </div>
@@ -18,38 +25,62 @@
 <script>
 
   import axios from 'axios'
-  import { reactive } from "vue";
+  //import { reactive } from "vue";
   //import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
   //import VueAxios from 'vue-axios'
 
-  //const apiURL = 'http://services.cineserie.com/v1/search/movies?q=${filmName}';
+  const apiURL = 'http://services.cineserie.com/v1/search/movies?q=';
 
   export default {
     name: 'films',
+    data() {
+      return {
+        Film: [],
+        title: "",
+        url: "",
+        videos: {
+          ba: {
+            link: ""
+          },
+        },
+        searchQuery: null,
+        searchDate: null,
+
+      };
+    },
 
     methods: {
-      getFilm() {
-        const state = reactive({
-          searchResults: [],
-          loading: false,
-        });
-
-        const fetchSearchResults = async (name) => {
-          if(name){
-            const res = axios.get(
-                    `http://services.cineserie.com/v1/search/movies?q=${name}`
-            );
-            if (res.data) {
-              state.searchResults = res.data?.title;
-            }
-          }
-
-        };
-        return {
-          state,
-          fetchSearchResults,
-        };
+      getFilms: function () {
+        axios.get(apiURL + this.searchQuery + "&release_date=" + this.searchDate)
+                .then(response => {
+                  this.Film = response.data.message
+                })
+                .catch(e => {
+                  this.errors = e
+                })
       },
+//      getFilm() {
+//        const state = reactive({
+//          searchResults: [],
+//          loading: false,
+//        });
+//
+//        const fetchSearchResults = async (name) => {
+//          if(name){
+//            const res = axios.get(
+//                    `http://services.cineserie.com/v1/search/movies?q=${name}`
+//            );
+//            if (res.data) {
+//              state.searchResults = res.data?.title;
+//            }
+//          }
+//
+//        };
+//        return {
+//          state,
+//          fetchSearchResults,
+//        };
+//      },
     },
   };
 
--- src/views/Series.vue
+++ src/views/Series.vue
@@ -2,9 +2,11 @@
     <div class="series">
         <h2>Séries</h2>
         <div id="demo" class="container">
-            <input type="text" placeholder="Entrez un nom de série" id="name">
-            <button type="button" onclick="getSerie();">Envoyer les informations</button>
-
+            <label for="name">Entrez un nom de série</label> <br>
+            <input type="text" v-model="searchQuery" placeholder="ex: The Walking Dead" id="name">
+            <br> <br>
+            <button type="button" @click="getSerie">Envoyer les informations</button>
+            <hr>
         </div>
         <p>
             <!--{{ getSerie() }}-->
@@ -18,37 +20,38 @@
 <script>
 
     import axios from 'axios'
-    import { reactive } from "vue";
+    //import { reactive } from "vue";
     //import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js'
     //import VueAxios from 'vue-axios'
 
-    //const apiURL = 'http://services.cineserie.com/v1/search/series?q=${name}';
+    const apiURL = 'http://services.cineserie.com/v1/search/series?q=';
 
     export default {
         name: 'series',
+        data() {
+            return {
+                Serie: [],
+                title: "",
+                url: "",
+                videos: {
+                    ba: {
+                        link: ""
+                    },
+                },
+                searchQuery: null,
+
+            };
+        },
 
         methods: {
-            getSerie() {
-                const state = reactive({
-                    searchResults: [],
-                    loading: false,
-                });
-
-                const fetchSearchResults = async (name) => {
-                    if(name){
-                        const res = axios.get(
-                            `http://services.cineserie.com/v1/search/series?q=${name}`
-                        );
-                        if (res.data) {
-                            state.searchResults = res.data?.title;
-                        }
-                    }
-
-                };
-                return {
-                    state,
-                    fetchSearchResults,
-                };
+            getSerie: function () {
+                axios.get(apiURL + this.searchQuery)
+                    .then(response => {
+                        this.Serie = response.data.message
+                    })
+                    .catch(e => {
+                        this.errors = e
+                    })
             },
         },
     };
